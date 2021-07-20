db0a60219ad38bc5dd537dfc2d33fbdf6ac288e2
VampirePrin4jc <katie.lotzer@gmail.com>
2021-07-19 23:16:31 -0500

    removed space

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import React from "react";
+
 import "./App.css";
 
 export default function App() {
@@ -7,7 +9,7 @@ export default function App() {
       <footer>
         This project was created by Katie Lotzer and is open-sourced on{" "}
         <a
-          href="https://github.com/VampirePrin4jc/react-weather-project "
+          href="https://github.com/VampirePrin4jc/react-weather-project"
           target="_blank"
           rel="noreferrer"
         >
