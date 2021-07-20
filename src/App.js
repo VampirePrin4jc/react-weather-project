12edf85ec91d1bd1a53baecdbc6fa6c553ba2de4
VampirePrin4jc <katie.lotzer@gmail.com>
2021-07-19 23:12:34 -0500

    added source code link

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        This project was created by Katie Lotzer and is open-sourced on
+        <a
+          href="https://github.com/VampirePrin4jc/react-weather-project "
+          target="_blank"
+        >
+          Github
+        </a>
+      </footer>
+    </div>
+  );
 }
