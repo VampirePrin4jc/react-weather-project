263de30c1dfc97a9afafbe59db812728b58f2959
VampirePrin4jc <katie.lotzer@gmail.com>
2021-07-19 23:29:20 -0500

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -5,17 +5,19 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      Weather App
-      <footer>
-        This project was created by Katie Lotzer and is open-sourced on{" "}
-        <a
-          href="https://github.com/VampirePrin4jc/react-weather-project"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Github
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was created by Katie Lotzer and is open-sourced on{" "}
+          <a
+            href="https://github.com/VampirePrin4jc/react-weather-project"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
